8a34cd1bd59342859b26afb066d34d03174f4c9f
Mahektek123 <mahektek63@gmail.com>
2024-06-13 01:44:14 +0530

    Updated

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,29 +2,41 @@ import React, { useEffect } from 'react'
 import Image from 'next/image'
 
 const Card = (Props: any) => {
-    const container = document.getElementsByClassName("container")
-    let ele = container[0] as HTMLElement
-    const bgGray = () => {
-        ele.classList.add("bgGray")
-    }
-    const removeGray = () => {
-        ele.classList.remove("bgGray")
-    }
+    
+    useEffect(()=> {
+        const container = document?.getElementsByClassName("container")
+        console.log("container ",container)
+        let ele = container[0] as HTMLElement
+        console.log("ele ", ele)
+            const bgGray = () => { 
+            ele?.classList.add("bgGray")
+        }
+        const removeGray = () => {
+            ele?.classList.remove("bgGray")
+        }
+    
+        const turnBlack = () => {
+            ele?.classList.add("bgblack")
+        }
+        const removeBlack = () => {
+            ele?.classList.remove("bgblack")
+        }
 
-    const turnBlack = () => {
-        ele.classList.add("bgblack")
-    }
-    const removeBlack = () => {
+        if (Props.isEven) {
+            ele.addEventListener('mouseenter', turnBlack);
+            ele.addEventListener('mouseleave', removeBlack);
+        } else {
+            ele.addEventListener('mouseenter', bgGray);
+            ele.addEventListener('mouseleave', removeGray);
+        }
+    },[])
 
-        ele.classList.remove("bgblack")
-    }
-
-
+   
 
     return (
         <>
-            <div className={`container ${Props.isEven}`}>
-            {/* <div className={`container ${Props.isEven}`} onMouseEnter={Props.isEven ? turnBlack : bgGray} onMouseLeave={Props.isEven ? removeBlack : removeGray}> */}
+            <div className="container">
+            {/* <div className={`container `} onMouseEnter={Props.isEven ? turnBlack : bgGray} onMouseLeave={Props.isEven ? removeBlack : removeGray}> */}
                 <div className='CardTitle'>
                     <h2><b> {Props.Title}</b></h2>
                 </div>
