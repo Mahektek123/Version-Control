192f93bfabd302556172fbe84b0c60e9492ec2e2
Mahektek123 <mahektek63@gmail.com>
2024-06-12 23:08:17 +0530

    Updated

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -26,7 +26,7 @@ const Card = (Props: any) => {
                         <h2><b> {Props.Title}</b></h2>
                     </div>
                     <div className='CardImg'>
-                        <Image priority src={Props.Img} width={200} height={150} alt="Peacock Image"></Image>
+                        <Image priority src={Props.Img} width={Props.isEven ? 200 : 270} height={150} alt="Peacock Image"></Image>
                     </div>
                     <div className='CardDesc'>
                         <p>{Props.Desc}</p>
